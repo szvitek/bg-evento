253e3a8bfe9e05a1ee3e00f4e5b254ac4e06f509
szvitek <gy.szvitek@gmail.com>
2024-04-23 22:33:06 +0100

    feat(Header): active links, clsx, framer-motion

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Link from 'next/link';
 import Logo from './logo';
-import Home from '@/app/page';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
 
 const routes = [
   { name: 'Home', path: '/' },
@@ -8,17 +12,31 @@ const routes = [
 ];
 
 export default function Header() {
+  const activePathname = usePathname();
   return (
     <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 text-sm h-full">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white transition"
+              className={clsx(
+                'hover:text-white transition relative flex items-center',
+                {
+                  'text-white': activePathname === route.path,
+                  'text-white/50': activePathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
