e38fd4763af17effd78c49474692a9a9f11d819c
szvitek <gy.szvitek@gmail.com>
2024-04-23 22:47:35 +0100

    feat(SearchForm): client component search form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchForm from '@/components/search-form';
 import Link from 'next/link';
 
 export default function Home() {
@@ -14,13 +15,7 @@ export default function Home() {
         around you
       </p>
 
-      <form className="w-full sm:w-[580px]">
-        <input
-          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
-          placeholder="Search events in any city..."
-          spellCheck={false}
-        />
-      </form>
+      <SearchForm />
 
       <section className='mt-4 flex gap-x-4 text-sm text-white/50'>
         <p>Populat:</p>
