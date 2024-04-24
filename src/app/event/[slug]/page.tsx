7610ee6bed11cbf3dc96f6d71e6b6aec6a53b21b
szvitek <gy.szvitek@gmail.com>
2024-04-24 11:16:51 +0100

    feat(EventPage): finish the event page, status-effects css

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import H1 from '@/components/h1';
 import { EventoEvent } from '@/lib/types';
 import Image from 'next/image';
+import React from 'react';
 
 type EventPageProps = {
   params: {
@@ -14,7 +15,6 @@ export default async function EventPage({ params }: EventPageProps) {
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
   );
   const event: EventoEvent = await response.json();
-  console.log(event);
 
   return (
     <main>
@@ -52,14 +52,40 @@ export default async function EventPage({ params }: EventPageProps) {
             <p className="whitespace-nowrap text-xl text-white/75">
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
-            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full py-2 rounded-md border-white/10 border-2 bg-blur hover:scale-105 focus:scale-105 active:scale-[1.02] transition">
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full py-2 rounded-md border-white/10 border-2 bg-blur state-effects">
               Get tickets
             </button>
           </div>
         </div>
       </section>
 
-      <div></div>
+      <div className="text-center px-5 py-16 min-h-[75vh]">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
     </main>
+  );
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="text-lg leading-8 text-white/75 max-w-4xl mx-auto">
+      {children}
+    </p>
   );
 }
--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -12,7 +12,7 @@ export default function EventCard({ event }: EventCardProps) {
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
     >
-      <section className="w-full h-full relative flex flex-col bg-white/[3%] rounded-xl overflow-hidden transition hover:scale-105 active:scale-[1.02]">
+      <section className="w-full h-full relative flex flex-col bg-white/[3%] rounded-xl overflow-hidden state-effects">
         <Image
           src={event.imageUrl}
           alt={event.name}
