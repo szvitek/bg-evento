f5acf119ad3a768acfb49c2caf7731c3b1f1ac3b
szvitek <gy.szvitek@gmail.com>
2024-04-24 10:49:31 +0100

    feat(EventPage): fetch data, event header

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,65 @@
-export default function EventPage() {
-  return <main>Event page</main>;
+import H1 from '@/components/h1';
+import { EventoEvent } from '@/lib/types';
+import Image from 'next/image';
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const { slug } = params;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event: EventoEvent = await response.json();
+  console.log(event);
+
+  return (
+    <main>
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
+        <Image
+          className="object-cover blur-3xl z-0"
+          src={event.imageUrl}
+          alt="Event background image"
+          fill
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          quality={50}
+          priority
+        />
+
+        <div className="relative z-1 flex flex-col lg:flex-row gap-6 lg:gap-16">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString('en-US', {
+                weekday: 'long',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full py-2 rounded-md border-white/10 border-2 bg-blur hover:scale-105 focus:scale-105 active:scale-[1.02] transition">
+              Get tickets
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
