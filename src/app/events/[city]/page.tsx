1452947ba06b32208d080589f1a1036617360f04
szvitek <gy.szvitek@gmail.com>
2024-04-25 13:24:07 +0100

    feat(Error/Validation): error handling page, and page params validation with zod

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react';
 import Loading from './loading';
 import { capitalize } from '@/lib/utils';
 import { Metadata } from 'next';
+import { z } from 'zod';
 
 type Props = {
   params: {
@@ -23,12 +24,17 @@ export function generateMetadata({ params }: Props): Metadata {
   };
 }
 
+const pageNumberSchema = z.coerce.number().int().positive().optional()
+
 export default async function EventsPage({
   params,
   searchParams,
 }: EventsPageProps) {
   const { city } = params;
-  const page = searchParams.page || 1;
+  const parsedPage = pageNumberSchema.safeParse(searchParams.page)
+  if (!parsedPage.success) {
+    throw new Error("Invalid page number")
+  }
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -37,8 +43,8 @@ export default async function EventsPage({
         {city !== 'all' && `Events in ${capitalize(city)}`}
       </H1>
 
-      <Suspense key={city + page} fallback={<Loading />}>
-        <EventsList city={city} page={+page} />
+      <Suspense key={city + parsedPage.data} fallback={<Loading />}>
+        <EventsList city={city} page={parsedPage.data} />
       </Suspense>
     </main>
   );
